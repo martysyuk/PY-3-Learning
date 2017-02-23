6bf060455ed9f026b498bce74f165a3045ab4f65
Azoff <martysyuk@gmail.com>
2017-02-24 01:01:45 +0700

    Переименовал переменные
    Переиеновал переменные и имена функций в более короткие, чтобы сократить
    длинну строк.

--- homeworks/lesson1-5.py
+++ homeworks/lesson1-5.py
@@ -17,53 +17,52 @@ students = [
      'experience': False, 'evaluation': (9, 8, 9, 5, 2), 'exam': 5}
 ]
 
-count_of_students = len(students)
-count_of_evaluations = len(students[0]['evaluation'])
+students_count = len(students)
+count_of_eval = len(students[0]['evaluation'])
 
 
-def average_evaluations():
-    count_of_all_evaluation = 0
-    count_of_all_exam = 0
+def average_evals():
+    count_all_evals = 0
+    count_all_exam = 0
 
     for student in students:
-        count_of_all_evaluation += sum(student['evaluation'])
-        count_of_all_exam += student['exam']
+        count_all_evals += sum(student['evaluation'])
+        count_all_exam += student['exam']
+        count_all_evals = round(count_all_evals / (students_count * count_of_eval), 1)
+        count_all_exam = round(count_all_exam / students_count, 1)
+    return count_all_evals, count_all_exam
 
-        count_of_all_evaluation = round(count_of_all_evaluation / (count_of_students * count_of_evaluations), 1)
-        count_of_all_exam = round(count_of_all_exam / count_of_students, 1)
-    return count_of_all_evaluation, count_of_all_exam
 
-
-def average_ratings_count(what_count, value):
-    count_evaluations = 0
+def average_rate_count(what_count, value):
+    count_evals = 0
     count_exams = 0
     count_filtered_students = 0
 
     for student in students:
         if what_count == 'sex':
             if student['sex'] == value == 'm':
-                count_evaluations += sum(student['evaluation'])
+                count_evals += sum(student['evaluation'])
                 count_exams += student['exam']
                 count_filtered_students += 1
             if student['sex'] == value == 'f':
-                count_evaluations += sum(student['evaluation'])
+                count_evals += sum(student['evaluation'])
                 count_exams += student['exam']
                 count_filtered_students += 1
         elif what_count == 'exp':
             if student['experience'] == value == True:
-                count_evaluations += sum(student['evaluation'])
+                count_evals += sum(student['evaluation'])
                 count_exams += student['exam']
                 count_filtered_students += 1
             if student['experience'] == value == False:
-                count_evaluations += sum(student['evaluation'])
+                count_evals += sum(student['evaluation'])
                 count_exams += student['exam']
                 count_filtered_students += 1
 
-    count_evaluations = round(count_evaluations
+    count_evals = round(count_evals
                               / (count_filtered_students
-                                 * count_of_evaluations), 1)
+                                 * count_of_eval), 1)
     count_exams = round(count_exams / count_filtered_students, 1)
-    return count_evaluations, count_exams
+    return count_evals, count_exams
 
 
 def best_students():
@@ -72,31 +71,29 @@ def best_students():
 
     for student in students:
         best_counting_formula = (0.6 * (sum(student['evaluation'])
-                                        / count_of_evaluations)) \
+                                        / count_of_eval)) \
                                 + (0.4 * student['exam'])
         average_students_rating.append(best_counting_formula)
     for index, bestStudent in enumerate(average_students_rating):
         if average_students_rating[index] == max(average_students_rating):
             best_students_list += '\n- ' + (students[index]['name'] + ' ' + students[index]['surname'])
-            # bestStudentsList сделал строкой, так как в дальнейшем использование этого списка не запланировано
-            # и строка выводится красивее. Уверен есть другой способ, но не стал думать :)
 
     return best_students_list
 
 
 print('\n' * 100)
 print('Средние отценки:\n- Домашние задания: {}\n- Экзамен: {}'
-      .format(average_evaluations()[0], average_evaluations()[1]))
+      .format(average_evals()[0], average_evals()[1]))
 print('\nСредняя отценка Мужчин\n- Домашние задания: {}\n- Экзамен: {}'
-      .format(average_ratings_count('sex', 'm')[0],
-              average_ratings_count('sex', 'm')[1]))
+      .format(average_rate_count('sex', 'm')[0],
+              average_rate_count('sex', 'm')[1]))
 print('\nСредняя отценка Женщин\n- Домашние задания: {}\n- Экзамен: {}'
-      .format(average_ratings_count('sex', 'f')[0],
-              average_ratings_count('sex', 'f')[1]))
+      .format(average_rate_count('sex', 'f')[0],
+              average_rate_count('sex', 'f')[1]))
 print('\nСредняя отценка студентов с Опытом\n- : Домашние задания: {}'
-      '\n- Экзамен: {}'.format(average_ratings_count('exp', True)[0],
-                               average_ratings_count('exp', True)[1]))
+      '\n- Экзамен: {}'.format(average_rate_count('exp', True)[0],
+                               average_rate_count('exp', True)[1]))
 print('\nСредняя отценка студентов без Опыта\n- : Домашние задания: {}'
-      '\n- Экзамен: {}'.format(average_ratings_count('exp', False)[0],
-                               average_ratings_count('exp', False)[1]))
+      '\n- Экзамен: {}'.format(average_rate_count('exp', False)[0],
+                               average_rate_count('exp', False)[1]))
 print('\nЛучший(е) студент(ы): {}'.format(best_students()))
