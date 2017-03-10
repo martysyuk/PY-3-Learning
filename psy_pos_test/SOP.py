719ff56a223a9fc21f9727a131ebcf6aaf52c7e8
Azoff <martysyuk@gmail.com>
2017-03-10 16:11:56 +0700

    Обработчик теста "Склонность к отклоняющемуся поведению (Орлов)"

--- psy_pos_test/SOP.py
+++ psy_pos_test/SOP.py
@@ -44,75 +44,41 @@ def input_new_data():
     input_data = dict()
     while True:
         name = input('Имя тестируемого: ')
-        if name == '': break
+
+        if name == '':
+            break
+
         age = input('Возраст: ')
         sex = input('Пол (м/ж): ')
         chooses = input('Номера отмеченных ответов через запятую (безпробелов):\n').rstrip().split(',')
 
-        input_data.update({name: {'Возреаст': age, 'Пол': sex, 'Выбор': chooses, 'social_orient_balance': 0,
-                                  'over_rules_balance': 0, 'addictive_behavior': 0, 'self_harm_behavior': 0,
-                                  'aggression_behavior': 0, 'emotional_reaction': 0, 'delinquency_behavior': 0,
-                                  'women_social_role': 0}})
-
+        if sex == "м":
+            input_data.update({name: {'Возреаст': age, 'Пол': sex, 'Выбор': chooses, 'social_orient_balance': 0,
+                                      'over_rules_balance': 0, 'addictive_behavior': 0, 'self_harm_behavior': 0,
+                                      'aggression_behavior': 0, 'emotional_reaction': 0, 'delinquency_behavior': 0}})
+        elif sex == "ж":
+            input_data.update({name: {'Возреаст': age, 'Пол': sex, 'Выбор': chooses, 'social_orient_balance': 0,
+                                      'over_rules_balance': 0, 'addictive_behavior': 0, 'self_harm_behavior': 0,
+                                      'aggression_behavior': 0, 'emotional_reaction': 0, 'delinquency_behavior': 0,
+                                      'women_social_role': 0}})
     return input_data
 
 
 def counting_individual_balls(data, person):
-    for i in range(len(data[person]['Выбор'])):
-        if data[person]['Пол'] == 'м':  # Подсчет балов для Мальчиков
-            for key in keys[]:
-                if (int(data[person]['Выбор'][i]) in key['has_it']) | \
-                        (int(data[person]['Выбор'][i]) not in key['has_not']):
+    if data[person]['Пол'] == 'м':
+        for key in keys['boys_keys']:
+            data[person][key] = 0
+        for choose in data[person]['Выбор']:
+            for key in keys['boys_keys']:
+                if (int(choose) in keys['boys']['has_it'][key]) | (int(choose) not in keys['boys']['has_not'][key]):
                     data[person][key] += 1
-
-            # if (int(data[person]['Выбор'][i]) in boys_social_orient_keys_yes) | \
-            #         (int(data[person]['Выбор'][i]) not in boys_social_orient_keys_no):
-            #     key_balls[person]['social_orient_balance'] += 1
-            # if (int(data[person]['Выбор'][i]) in boys_over_rules_keys_yes) | \
-            #         (int(data[person]['Выбор'][i]) not in boys_over_rules_keys_no):
-            #     key_balls[person]['over_rules_balance'] += 1
-            # if (int(data[person]['Выбор'][i]) in boys_addictive_behavior_keys_yes) | \
-            #         (int(data[person]['Выбор'][i]) not in boys_addictive_behavior_keys_no):
-            #     key_balls[person]['addictive_behavior'] += 1
-            # if (int(data[person]['Выбор'][i]) in boys_self_harm_behavior_keys_yes) | \
-            #         (int(data[person]['Выбор'][i]) not in boys_self_harm_behavior_keys_no):
-            #     key_balls[person]['self_harm_behavior'] += 1
-            # if (int(data[person]['Выбор'][i]) in boys_aggression_behavior_keys_yes) | \
-            #         (int(data[person]['Выбор'][i]) not in boys_aggression_behavior_keys_no):
-            #     key_balls[person]['aggression_behavior'] += 1
-            # if (int(data[person]['Выбор'][i]) in boys_emotional_reaction_keys_yes) | \
-            #         (int(data[person]['Выбор'][i]) not in boys_emotional_reaction_keys_no):
-            #     key_balls[person]['emotional_reaction'] += 1
-            # if (int(data[person]['Выбор'][i]) in boys_delinquency_behavior_keys_yes) | \
-            #         (int(data[person]['Выбор'][i]) not in boys_delinquency_behavior_keys_no):
-            #     key_balls[person]['delinquency_behavior'] += 1
-
-        if data[person]['Пол'] == 'ж':  # Подсчет балов для Девочек
-            print('sss')
-            # if (int(data[person]['Выбор'][i]) in girls_social_orient_keys_yes) | \
-            #         (int(data[person]['Выбор'][i]) not in girls_social_orient_keys_no):
-            #     key_balls[person]['social_orient_balance'] += 1
-            # if (int(data[person]['Выбор'][i]) in girls_over_rules_keys_yes) | \
-            #         (int(data[person]['Выбор'][i]) not in girls_over_rules_keys_no):
-            #     key_balls[person]['over_rules_balance'] += 1
-            # if (int(data[person]['Выбор'][i]) in girls_addictive_behavior_keys_yes) | \
-            #         (int(data[person]['Выбор'][i]) not in girls_addictive_behavior_keys_no):
-            #     key_balls[person]['addictive_behavior'] += 1
-            # if (int(data[person]['Выбор'][i]) in girls_self_harm_behavior_keys_yes) | \
-            #         (int(data[person]['Выбор'][i]) not in girls_self_harm_behavior_keys_no):
-            #     key_balls[person]['self_harm_behavior'] += 1
-            # if (int(data[person]['Выбор'][i]) in girls_aggression_behavior_keys_yes) | \
-            #         (int(data[person]['Выбор'][i]) not in girls_aggression_behavior_keys_no):
-            #     key_balls[person]['aggression_behavior'] += 1
-            # if (int(data[person]['Выбор'][i]) in girls_emotional_reaction_keys_yes) | \
-            #         (int(data[person]['Выбор'][i]) not in girls_emotional_reaction_keys_no):
-            #     key_balls[person]['emotional_reaction'] += 1
-            # if (int(data[person]['Выбор'][i]) in girls_delinquency_behavior_keys_yes) | \
-            #         (int(data[person]['Выбор'][i]) not in girls_delinquency_behavior_keys_no):
-            #     key_balls[person]['delinquency_behavior'] += 1
-            # if (int(data[person]['Выбор'][i]) in women_social_role_keys_yes) | \
-            #         (int(data[person]['Выбор'][i]) not in women_social_role_keys_no):
-            #     key_balls[person]['women_social_role'] += 1
+    if data[person]['Пол'] == 'ж':
+        for key in keys['girls_keys']:
+            data[person][key] = 0
+        for choose in data[person]['Выбор']:
+            for key in keys['girls_keys']:
+                if (int(choose) in keys['girls']['has_it'][key]) | (int(choose) not in keys['girls']['has_not'][key]):
+                    data[person][key] += 1
 
 
 def choose_menu(data):
@@ -120,7 +86,7 @@ def choose_menu(data):
     print('МЕНЮ ПРОГРАММЫ:\n\n')
     print('1. Вывести список имеющихся респондентов')
     print('2. Добавить новые данные')
-    print('3. Вывести результаты тестирования')
+    print('3. Подсчитать результаты тестирования')
     print('q. Выход')
     choose = 0
 
@@ -140,8 +106,6 @@ def choose_menu(data):
             if data != {}:
                 save_data_to_json_file(file_name, data)
                 print('\nФАЙЛ УСПЕШНО СОХРАНЕН.')
-            elif data == load_data:
-                print('\nНЕ БЫЛО ВНЕСЕНО НОВЫХ ДАННЫХ')
             break
 
 
