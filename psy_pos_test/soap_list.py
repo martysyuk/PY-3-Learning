3d52c63de33f2e6cb2b84e5a6f1e2cd56fe45340
Azoff <martysyuk@gmail.com>
2017-03-19 22:54:03 +0700

    Чистка репозитория

--- psy_pos_test/soap_list.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf8 -*-
-
-import json
-
-with open('12-7a.json', 'r', encoding='utf8') as f:
-    load_data = json.loads(f.read())
-    for person in load_data:
-        print(person+', {} лет'.format(load_data[person]['Возреаст']))
