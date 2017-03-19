b4a30d061494c05ca2591233f2e844f2c8539253
Azoff <martysyuk@gmail.com>
2017-03-19 22:38:19 +0700

    Добавил ДЗ 2.5 «Работа с папками, путями»

--- homeworks/lesson2-5.py
+++ homeworks/lesson2-5.py
@@ -6,21 +6,36 @@
 import glob
 from os.path import join
 
-work_dir = ['lesson2-5','homework','Advanced']
+
+def search_in_files(search, files_list):
+    temp_filtered_list = list()
+    for index, file in enumerate(files_list):
+        with open(file, 'r') as work_file:
+            data = work_file.read().lower()
+            if search in data:
+                temp_filtered_list.append(file)
+
+    return temp_filtered_list
+
+
+def print_result(data):
+    file_count = 0
+    print('\nИскомая фраза встрачается в файлах:')
+    for index, data_stdout in enumerate(data):
+        print(str(index+1), ')', data_stdout)
+        file_count += 1
+    print('Всего найденых файлов: {}\n'.format(file_count))
+
+
+work_dir = ['lesson2-5', 'homework', 'Advanced']
 files = glob.glob(join(*work_dir, '*.sql'))
 filtered_list = files
 
 
-# def filter_files(filter):
-#     filter = '*' + filter + '*.sql'
-#     files = glob.glob(join(*work_dir, filter))
-#     for index, file in enumerate(files):
-#         print(str(index+1)+') '+file)
-#     print('Всего {} файлов.'.format(str(len(files))))
-
 while True:
-    user_filter = input('Введите фильтр: ')
-    if user_filter == '':
+    search_str = input('Введите искомое слово: ')
+    if search_str == '':
         exit(0)
-    filtered_list = filter(lambda x: x.find(user_filter), files)
-    print(filtered_list)
+    search_str = search_str.lower()
+    filtered_list = search_in_files(search_str, filtered_list)
+    print_result(filtered_list)
