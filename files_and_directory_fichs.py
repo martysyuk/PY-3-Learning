da57cc6cd58c5f61634ad4f404bb210be9ed1d77
Azoff <martysyuk@gmail.com>
2017-03-20 00:10:16 +0700

    Обновление списка фич работы с файлами ипапками

--- files_and_directory_fichs.py
+++ files_and_directory_fichs.py
@@ -2,24 +2,43 @@
 import os
 import glob
 # Можно импортировать только конкретные функции из библиотеки, для сокращения вызова их в коде.
-from os.path import join, dirname, realpath
+from os.path import join, dirname
+# Или можно импортировать функцию как переменную
+import os.path as path
 
 # Задаем пусть в вие последовательности папок в списке
-path = ['home', 'user', 'ivan']
-print(path)
-print(*path)
+work_path = ['home', 'user', 'ivan']
+print(work_path)
+print(*work_path)
+
 # Объеденяем именя папок в путь формата операционной системы (для каждлой свой формат)
-new_path = join(*path) # А можно написать new_path = os.path.join
+new_path = join(*work_path)  # А можно написать new_path = os.path.join
 print(new_path)
+
 # Выводим путь и имя текущего (в катором написан данный код) файла
 print(__file__)
+
 # Задаем переменной полнй путь до текущего файла
-dir_name = dirname(realpath(__file__))
+dir_name = dirname(path.realpath(__file__))
 print(dir_name)
+
 # Добавляем к текущему пути папку выше
 dir_name = join(dir_name, 'homeworks')
 print(dir_name)
+
 # Меняем текущую (рабочую) папку на новую
 os.chdir(dir_name)
+
 # выводим списко файлов в текущей папке по фильтру
 print(glob.glob('*.py'))
+
+# получение списка файлов определенного расширения.
+image_file_ext = ('jpg', 'png', 'jpeg', 'gif', 'bmp')
+image_files = list()
+working_path = 'C:\Windows\System32'
+for file in os.listdir(working_path):
+    if file.endswith(image_file_ext):
+        image_files.append(file)
+if len(image_files) == 0:
+    print('no image files in the directory: "' + working_path + '"')
+print(image_files)
