2023f6b480d951e34b7d8e3f10dde51ef65ed5a6
Azoff <martysyuk@gmail.com>
2017-03-25 23:53:24 +0700

    Обновил ДЗ 2.6

--- homeworks/lesson2-6.py
+++ homeworks/lesson2-6.py
@@ -13,21 +13,27 @@ def get_image_files_list(sub_dir):
     work_dir = dirname(realpath(__file__))
     images_path = join(work_dir, sub_dir)
     os.chdir(images_path)
-    return glob.glob('*.jpg')
+    files = glob.glob('*.jpg')
+    os.chdir(work_dir)
+    return files
 
 
-def subprocess_run(image_files):
+def subprocess_run(image_files, source_dir, result_dir):
     for file_name in image_files:
-        done_file_name = 'resize-'+file_name
+        get_file_name = join(source_dir, file_name)
+        done_file_name = join(result_dir, file_name)
         # convert input.jpg -resize 200 output.jpg
-        process = Popen(['convert', file_name, '-resize', '200', done_file_name], stdout=PIPE, stderr=PIPE)
+        process = Popen(['convert', get_file_name, '-resize', '200', done_file_name])#, stdout=PIPE, stderr=PIPE)
         process.communicate()
         if process.returncode != 0:
             print('ОШИБКА конвертации файла {}.'.format(file_name))
+            print(process.returncode)
         else:
             print('Конвертация файла {} успешно завершина'.format(file_name))
 
 
-sub_folder_name = 'lesson2-6'
-subprocess_run(get_image_files_list(sub_folder_name))
+source_folder = 'Source'
+result_folder = 'Result'
+files_list = get_image_files_list(source_folder)
+subprocess_run(files_list, source_folder, result_folder)
 
