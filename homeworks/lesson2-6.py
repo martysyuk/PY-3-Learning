3e34317875c4d4a7d72782e63d85321bdcae4348
martysyuk <martysyuk@gmail.com>
2017-03-27 12:38:49 +0700

    Добавил файл ДЗ 2-7

--- homeworks/lesson2-6.py
+++ homeworks/lesson2-6.py
@@ -5,7 +5,7 @@
 
 import os
 import glob
-from os.path import join, dirname, realpath
+from os.path import join, dirname, realpath, exists
 from subprocess import Popen, PIPE
 
 
@@ -15,7 +15,11 @@ def get_image_files_list(sub_dir):
     os.chdir(images_path)
     files = glob.glob('*.jpg')
     os.chdir(work_dir)
-    return files
+    if files == list():
+        print('В папке \"{}\" файлов не найдено!'.format(images_path))
+        exit(0)
+    else:
+        return files
 
 
 def subprocess_run(image_files, source_dir, result_dir):
@@ -31,10 +35,24 @@ def subprocess_run(image_files, source_dir, result_dir):
             print('Конвертация файла {} успешно завершина'.format(file_name))
 
 
+def check_and_create_folder(chk_dir, what_is_dir):
+    if chk_dir == '':
+        chk_dir = input('Введите имя папки {}: '.format(what_is_dir))
+    chk_dir = join(dirname(realpath(__file__)), chk_dir)
+    if not exists(chk_dir):
+        os.mkdir(chk_dir)
+
+
 # Папки относительно директории с Python файлом.
 source_folder = 'Source'
 result_folder = 'Result'
 # Файл convert.exe должен лежать в тойже папке что и Python файл.
+
+print('Проверям папку с картинками...')
+check_and_create_folder(source_folder, 'с картинками')
+print('Проверяем папку назначения...')
+check_and_create_folder(result_folder, 'назначения')
+print('Программа готова к работе.\n')
 
 files_list = get_image_files_list(source_folder)
 subprocess_run(files_list, source_folder, result_folder)
